9a11c36fed7c5408e3154ba67054b0cef6c7e151
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-11-23 22:36:16 +0000

    Add implementations for functions

--- lexel.h
+++ lexel.h
@@ -1,6 +1,7 @@
 #ifndef LEXEL_H
 #define LEXEL_H
 
+#include <stddef.h>  // size_t
 
 // LEXEL CORE.
 
@@ -9,6 +10,7 @@
 struct lxl_lexer {
     const char *start;  // The start of the lexer's source code.
     const char *end;    // The end of the lexer's source code.
+    const char *current;
 };
 
 struct lxl_token {
@@ -51,6 +53,21 @@ int lxl_lexer_is_finished(struct lxl_lexer *lexer);
 // These functions are used by the lexer to alter its own state.
 // They should not be used from a parser, but the interface is exposed to make writing a custom lexer easier.
 
+
+// Return the number of characters consumed so far.
+size_t lxl_lexer__head_length(struct lxl_lexer *lexer);
+// Return the number of characters left in the lexer's source.
+size_t lxl_lexer__tail_length(struct lxl_lexer *lexer);
+
+// Return whether the lexer is at the end of its input.
+int lxl_lexer__is_at_end(struct lxl_lexer *lexer);
+// Return the current character and advance the lexer to the next character.
+char lxl_lexer__advance(struct lxl_lexer *lexer);
+// Advance the lexer by up to n characters and return whether all n characters could be advanced
+// (this will be false when there are fewer than n characters left, in which case, the lexer
+// reaches the end and stops).
+int lxl_lexer__advance_by(struct lxl_lexer *lexer, size_t n);
+
 // Return whether the current character matches any of those passed, but do not consume it.
 int lxl_lexer__check_chars(struct lxl_lexer *lexer, const char *chars);
 // Return whether the next characters match exactly the string passed, but do not consume them.
@@ -68,5 +85,83 @@ int lxl_lexer__match_string(struct lxl_lexer *lexer, const char *s);
 int lxl_lexer__match_string_n(struct lxl_lexer *lexer, const char *s, size_t n);
 
 // END LEXER INTERNAL INTERFACE.
+
+
+// Implementation.
+
+#ifdef LEXEL_IMPLEMENTATION
+
+#include <stdbool.h>
+
+size_t lxl_lexer__head_length(struct lxl_lexer *lexer) {
+    return lexer->current - lexer->start;
+}
+
+size_t lxl_lexer__tail_length(struct lxl_lexer *lexer) {
+    return lexer->end - lexer->current;
+}
+
+int lxl_lexer__is_at_end(struct lxl_lexer *lexer) {
+    return lexer->current >= lexer->end;
+}
+
+char lxl_lexer__advance(struct lxl_lexer *lexer) {
+    if (lxl_lexer__is_at_end(lexer)) return '\0';
+    ++lexer->current;
+    return lexer->current[-1];
+}
+
+int lxl_lexer__advance_by(struct lxl_lexer *lexer, size_t n) {
+    size_t tail_length = lxl_lexer__tail_length(lexer);
+    if (n < tail_length) {
+        lexer->current = lexer->end;
+        return false;
+    }
+    lexer->current += n;
+    return true;
+}
+
+int lxl_lexer__check_chars(struct lxl_lexer *lexer, const char *chars) {
+    while (*chars != '\0') {
+        if (*lexer->current == *chars) return true;
+        ++chars;
+    }
+    return false;
+}
+
+int lxl_lexer__check_string(struct lxl_lexer *lexer, const char *s) {
+    return strncmp(lexer->current, s, lexer->end - lexer->current) == 0;
+}
+
+int lxl_lexer__check_string_n(struct lxl_lexer *lexer, const char *s, size_t n) {
+    size_t tail_length = lxl_lexer__tail_length(lexer);
+    if (n < tail_length) n = tail_length;
+    return strncmp(lexer->current, s, n) == 0;
+}
+
+int lxl_lexer__match_chars(struct lxl_lexer *lexer, const char *chars) {
+    if (lxl_lexer__check_chars(lexer, chars)) {
+        return !!lxl_lexer__advance(lexer);
+    }
+    return false;
+}
+
+int lxl_lexer__match_string(struct lxl_lexer *lexer, const char *s) {
+    if (lxl_lexer__check_string(lexer, s)) {
+        size_t length = strlen(s);
+        return !!lxl_lexer__advance_by(lexer, length);
+    }
+    return false;
+}
+
+int lxl_lexer__match_string_n(struct lxl_lexer *lexer, const char *s, size_t n) {
+    if (lxl_lexer__check_string_n(lexer, s)) {
+        return !!lxl_lexer__advance_by(lexer, n);
+    }
+    return false;
+}
+
+#undef LEXEL_IMPLEMENTATION
+#endif // LEXEL_IMPLEMENTATION
 
 #endif  // LEXEL_H
