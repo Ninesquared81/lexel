5ac5f9e422457a5a06a539a8c1aecd646689ed44
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-11-23 23:31:02 +0000

    Call some lexer functions in example.c

--- example.c
+++ example.c
@@ -5,5 +5,9 @@
 
 int main(void) {
     printf("Hello, World!\n");
+    struct lxl_lexer lexer = lxl_lexer_from_sv(LXL_SV_FROM_STRLIT("1 2 +"));
+    for (int i = 0; !lxl_lexer__is_at_end(&lexer); ++i) {
+        printf("Character %d: '%c'\n", i, lxl_lexer__advance(&lexer));
+    }
     return 0;
 }
