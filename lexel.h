d72be21e5f45a258950c1434aafa5b21f0c62889
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-11-24 23:41:41 +0000

    Use `bool` type in obvious places
    This does mean that the library is restricted to C99+, but that's over
    25 years old, so it's a fair restriction.

--- lexel.h
+++ lexel.h
@@ -91,7 +91,7 @@ struct lxl_token lxl_lexer_next_token(struct lxl_lexer *lexer);
 
 // Return whether the token stream of the lexer is exhausted
 // (i.e. there are no more tokens in the source code).
-int lxl_lexer_is_finished(struct lxl_lexer *lexer);
+bool lxl_lexer_is_finished(struct lxl_lexer *lexer);
 
 // Reset the lexer to the start of its input.
 void lxl_lexer_reset(struct lxl_lexer *lexer);
@@ -111,29 +111,29 @@ size_t lxl_lexer__head_length(struct lxl_lexer *lexer);
 size_t lxl_lexer__tail_length(struct lxl_lexer *lexer);
 
 // Return whether the lexer is at the end of its input.
-int lxl_lexer__is_at_end(struct lxl_lexer *lexer);
+bool lxl_lexer__is_at_end(struct lxl_lexer *lexer);
 // Return the current character and advance the lexer to the next character.
 char lxl_lexer__advance(struct lxl_lexer *lexer);
 // Advance the lexer by up to n characters and return whether all n characters could be advanced
 // (this will be false when there are fewer than n characters left, in which case, the lexer
 // reaches the end and stops).
-int lxl_lexer__advance_by(struct lxl_lexer *lexer, size_t n);
+bool lxl_lexer__advance_by(struct lxl_lexer *lexer, size_t n);
 
 // Return whether the current character matches any of those passed, but do not consume it.
-int lxl_lexer__check_chars(struct lxl_lexer *lexer, const char *chars);
+bool lxl_lexer__check_chars(struct lxl_lexer *lexer, const char *chars);
 // Return whether the next characters match exactly the string passed, but do not consume them.
-int lxl_lexer__check_string(struct lxl_lexer *lexer, const char *s);
+bool lxl_lexer__check_string(struct lxl_lexer *lexer, const char *s);
 // Return whether the next n characters match the first n characters of the string passed,
 // but do not consume them.
-int lxl_lexer__check_string_n(struct lxl_lexer *lexer, const char *s, size_t n);
+bool lxl_lexer__check_string_n(struct lxl_lexer *lexer, const char *s, size_t n);
 
 // Return whether the current character matches any of those passed, and consume it if so.
-int lxl_lexer__match_chars(struct lxl_lexer *lexer, const char *chars);
+bool lxl_lexer__match_chars(struct lxl_lexer *lexer, const char *chars);
 // Return whether the next characters match exactly the string passed, and consume them if so.
-int lxl_lexer__match_string(struct lxl_lexer *lexer, const char *s);
+bool lxl_lexer__match_string(struct lxl_lexer *lexer, const char *s);
 // Return whether the next n characters match the first n characters of the string passed,
 // and consume them if so.
-int lxl_lexer__match_string_n(struct lxl_lexer *lexer, const char *s, size_t n);
+bool lxl_lexer__match_string_n(struct lxl_lexer *lexer, const char *s, size_t n);
 
 // Advance the lexer past any whitespace characters and return the number of characters consumed.
 int lxl_lexer__skip_whitespace(struct lxl_lexer *lexer);
@@ -193,7 +193,7 @@ size_t lxl_lexer__tail_length(struct lxl_lexer *lexer) {
     return lexer->end - lexer->current;
 }
 
-int lxl_lexer__is_at_end(struct lxl_lexer *lexer) {
+bool lxl_lexer__is_at_end(struct lxl_lexer *lexer) {
     return lexer->current >= lexer->end;
 }
 
@@ -203,7 +203,7 @@ char lxl_lexer__advance(struct lxl_lexer *lexer) {
     return lexer->current[-1];
 }
 
-int lxl_lexer__advance_by(struct lxl_lexer *lexer, size_t n) {
+bool lxl_lexer__advance_by(struct lxl_lexer *lexer, size_t n) {
     size_t tail_length = lxl_lexer__tail_length(lexer);
     if (n < tail_length) {
         lexer->current = lexer->end;
@@ -213,7 +213,7 @@ int lxl_lexer__advance_by(struct lxl_lexer *lexer, size_t n) {
     return true;
 }
 
-int lxl_lexer__check_chars(struct lxl_lexer *lexer, const char *chars) {
+bool lxl_lexer__check_chars(struct lxl_lexer *lexer, const char *chars) {
     while (*chars != '\0') {
         if (*lexer->current == *chars) return true;
         ++chars;
@@ -221,24 +221,24 @@ int lxl_lexer__check_chars(struct lxl_lexer *lexer, const char *chars) {
     return false;
 }
 
-int lxl_lexer__check_string(struct lxl_lexer *lexer, const char *s) {
+bool lxl_lexer__check_string(struct lxl_lexer *lexer, const char *s) {
     return strncmp(lexer->current, s, lexer->end - lexer->current) == 0;
 }
 
-int lxl_lexer__check_string_n(struct lxl_lexer *lexer, const char *s, size_t n) {
+bool lxl_lexer__check_string_n(struct lxl_lexer *lexer, const char *s, size_t n) {
     size_t tail_length = lxl_lexer__tail_length(lexer);
     if (n < tail_length) n = tail_length;
     return strncmp(lexer->current, s, n) == 0;
 }
 
-int lxl_lexer__match_chars(struct lxl_lexer *lexer, const char *chars) {
+bool lxl_lexer__match_chars(struct lxl_lexer *lexer, const char *chars) {
     if (lxl_lexer__check_chars(lexer, chars)) {
         return !!lxl_lexer__advance(lexer);
     }
     return false;
 }
 
-int lxl_lexer__match_string(struct lxl_lexer *lexer, const char *s) {
+bool lxl_lexer__match_string(struct lxl_lexer *lexer, const char *s) {
     if (lxl_lexer__check_string(lexer, s)) {
         size_t length = strlen(s);
         return !!lxl_lexer__advance_by(lexer, length);
@@ -246,7 +246,7 @@ int lxl_lexer__match_string(struct lxl_lexer *lexer, const char *s) {
     return false;
 }
 
-int lxl_lexer__match_string_n(struct lxl_lexer *lexer, const char *s, size_t n) {
+bool lxl_lexer__match_string_n(struct lxl_lexer *lexer, const char *s, size_t n) {
     if (lxl_lexer__check_string_n(lexer, s, n)) {
         return !!lxl_lexer__advance_by(lexer, n);
     }
