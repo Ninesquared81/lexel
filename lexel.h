ceeaf5d32811eb4260f4bdbe3c9f0ddec567e031
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-11-26 01:20:11 +0000

    Set `.is_finished` when the lexer emits a TOKENS_END token

--- lexel.h
+++ lexel.h
@@ -194,12 +194,12 @@ struct lxl_lexer lxl_lexer_from_sv(struct lxl_string_view sv) {
 
 struct lxl_token lxl_lexer_next_token(struct lxl_lexer *lexer) {
     lxl_lexer__skip_whitespace(lexer);
+    if (lxl_lexer__is_at_end(lexer)) {
+        return lxl_lexer__create_end_token(lexer);
+    }
     struct lxl_token token = lxl_lexer__start_token(lexer);
     lxl_lexer__lex_symbolic(lexer);
     lxl_lexer__finish_token(lexer, &token);
-    if (lxl_lexer__is_at_end(lexer)) {
-        lexer->is_finished = true;
-    }
     return token;
 }
 
@@ -304,6 +304,13 @@ struct lxl_token lxl_lexer__start_token(struct lxl_lexer *lexer) {
 
 void lxl_lexer__finish_token(struct lxl_lexer *lexer, struct lxl_token *token) {
     token->end = lexer->current;
+}
+
+struct lxl_lexer__create_end_token(struct lxl_lexer *lexer) {
+    struct lxl_token token = lxl_lexer__start_token(lexer);
+    token.token_type = LSL_TOKENS_END;
+    lexer->is_finished = true;
+    return token;
 }
 
 int lxl_lexer__lex_symbolic(struct lxl_lexer *lexer) {
