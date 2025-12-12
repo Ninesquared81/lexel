995a06e4f9a3d81893b4675e0317652824cdf244
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2025-12-12 17:01:51 +0000

    Move Unicode type definitions to section LEXEL ADDITIONAL

--- lexel.h
+++ lexel.h
@@ -216,6 +216,29 @@ struct lxl_string_view {
     ptrdiff_t length;
 };
 
+// A single UTF-8 codepoint.
+typedef uint32_t lxl_unicode_codepoint;
+
+// Error codes for Unicode functions.
+enum lxl_unicode_error {
+    LXL_UNIERR_OK = 0,
+    LXL_UNIERR_UNEXPECTED_EOF,          // Unexpected end of input (possibly in multibyte sequence).
+    LXL_UNIERR_INVALID_FIRST_BYTE,      // Invalid first byte for UTF-8 sequence.
+    LXL_UNIERR_INVALID_CONT_BYTE,       // Invalid continuation byte
+    LXL_UNIERR_UNEXPECTED_CONT_BYTE,    // Continuation byte at start of input.
+    LXL_UNIERR_MISSING_CONT_BYTE,       // Missing continuation byte(s) before start of next sequence.
+    LXL_UNIERR_OUT_OF_RANGE,            // Value correctly encoded but outside of Unicode range.
+    LXL_UNIERR_OVERLONG_ENCODING,       // Encoding is longer than necessary, possibly malicious.
+};
+
+// UTF-8 character stream
+struct lxl_unicode_utf8_stream {
+    struct lxl_string_view buffer;      // The backing buffer containing the characters for the stream.
+    ptrdiff_t cursor;                   // The current position in the stream.
+    enum lxl_unicode_error error;       // The latest error, cleared/set when the stream is advanced.
+};
+
+
 // END LEXEL ADDITIONAL.
 
 
@@ -274,28 +297,6 @@ const char *lxl_error_message(enum lxl_lex_error error);
 // UNICODE INTERFACE.
 
 // These functions are for working with Unicode/UTF-8, which is what lexel expects.
-
-// A single UTF-8 codepoint.
-typedef uint32_t lxl_unicode_codepoint;
-
-// Error codes for Unicode functions.
-enum lxl_unicode_error {
-    LXL_UNIERR_OK = 0,
-    LXL_UNIERR_UNEXPECTED_EOF,          // Unexpected end of input (possibly in multibyte sequence).
-    LXL_UNIERR_INVALID_FIRST_BYTE,      // Invalid first byte for UTF-8 sequence.
-    LXL_UNIERR_INVALID_CONT_BYTE,       // Invalid continuation byte
-    LXL_UNIERR_UNEXPECTED_CONT_BYTE,    // Continuation byte at start of input.
-    LXL_UNIERR_MISSING_CONT_BYTE,       // Missing continuation byte(s) before start of next sequence.
-    LXL_UNIERR_OUT_OF_RANGE,            // Value correctly encoded but outside of Unicode range.
-    LXL_UNIERR_OVERLONG_ENCODING,       // Encoding is longer than necessary, possibly malicious.
-};
-
-// UTF-8 character stream
-struct lxl_unicode_utf8_stream {
-    struct lxl_string_view buffer;      // The backing buffer containing the characters for the stream.
-    ptrdiff_t cursor;                   // The current position in the stream.
-    enum lxl_unicode_error error;       // The latest error, cleared/set when the stream is advanced.
-};
 
 // Get the tail (unconsumed characters) of a UTF-8 stream as a string view.
 struct lxl_string_view lxl_unicode_utf8_stream_tail(const struct lxl_unicode_utf8_stream *stream);
