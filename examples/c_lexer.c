8793697d7d398df3b0addcbc2c636900620794d9
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2026-05-27 23:32:34 +0100

    Update C lexer example

--- examples/c_lexer.c
+++ examples/c_lexer.c
@@ -5,8 +5,46 @@
 #include <ctype.h>
 
 int main(void) {
-    printf("Hello, Lexel!\n");
+    struct lxl_string_view src =
+        LXL_SV_FROM_STRLIT_INIT("   assert(1 + 1 == 2);");
+    struct lxl_lexer lexer = create_c_lexer(src);
+    for (struct lxl_token token; !LXL_TOKEN_IS_END(token = lxl_lexer_next_token(&lexer));) {
+        struct lxl_string_view token_sv = lxl_token_value(token);
+        // struct lxl_string_view kind_sv = c_token_kind_name(token.kind);
+        printf("%"LXL_SV_FMT_SPC"\n",
+               LXL_SV_FMT_ARG(token_sv));
+    }
     return 0;
+}
+
+struct lxl_lexer create_c_lexer(struct lxl_string_view src) {
+    struct lxl_lexer lexer = lxl_lexer_new(src);
+    // Query functions.
+    lexer.match_word_init_char = match_word_init_char;
+    lexer.match_word_char = match_word_char;
+    lexer.match_int_prefix = match_int_prefix;
+    lexer.match_punct = match_punct;
+    // Token type getters.
+    lexer.get_word_type = get_word_type;
+    lexer.get_int_type = get_int_type;
+    lexer.get_float_type = get_float_type;
+    lexer.get_punct_type = get_punct_type;
+    return lexer;
+}
+
+struct lxl_string_view c_token_kind_name(enum c_token_type type) {
+    static struct lxl_string_view names[] = {
+        C_TOKENS(C_TOKENS_STRING_TABLE)
+    };
+    LXL_ASSERT(0 <= type && type < sizeof names / sizeof names[0]);
+    return names[type];
+}
+
+bool match_word_init_char(struct lxl_lexer *self) {
+    lxl_UnicodeCodepoint ch = lxl_lexer__advance(self);
+    if (isalpha(ch) || ch == '_') return true;
+    lxl_lexer__rewind(self);
+    return false;
 }
 
 bool match_word_char(struct lxl_lexer *self) {
@@ -18,29 +56,23 @@ bool match_word_char(struct lxl_lexer *self) {
 
 bool match_int_prefix(struct lxl_lexer *self) {
     lxl_UnicodeCodepoint ch = lxl_lexer__advance(self);
+    self->match_int_digit = NULL;  // Use default integer lexer.
     if (ch == '0') {
         ch = lxl_lexer__advance(self);
-        if (ch != 'x' && ch != 'X' && ch != 'b' && ch != 'B') {
-            lxl_lexer__rewind(self);
+        if (toupper(ch) == 'X') {
+            // Hexadecimal.
+            self->match_int_digit = lxl_lexer__match_int_digit_builtin_hex;
+            return true;
         }
+        if (toupper(ch) != 'B') {
+            // Binary.
+            return true;
+        }
+        // Octal.
+        lxl_lexer__rewind(self);
         return true;
     }
     if ('1' <= ch && ch <= '9') return true;
-    lxl_lexer__rewind(self);
-    return false;
-}
-
-bool match_int_digit(struct lxl_lexer *self) {
-    lxl_UnicodeCodepoint ch = lxl_lexer__advance(self);
-    if (isxdigit(ch) || ch == '\'') return true;
-    lxl_lexer__rewind(self);
-    return false;
-}
-
-bool match_float_prefix(struct lxl_lexer *self) {
-    if (match_int_prefix(self)) return true;
-    lxl_UnicodeCodepoint ch = lxl_lexer__advance(self);
-    if (ch == '.') return true;
     lxl_lexer__rewind(self);
     return false;
 }
@@ -128,8 +160,23 @@ bool match_punct(struct lxl_lexer *self) {
     return false;
 }
 
+int get_word_type(struct lxl_lexer *self) {
+    (void)self;
+    return CTOK_IDENTIFIER;
+}
+
+int get_int_type(struct lxl_lexer *self) {
+    (void)self;
+    return CTOK_LIT_INT;
+}
+
+int get_float_type(struct lxl_lexer *self) {
+    (void)self;
+    return CTOK_LIT_FLOAT;
+}
+
 int get_punct_type(struct lxl_lexer *self) {
-    struct lxl_string_view token_sv = lxl_token_value(self->token);
+    struct lxl_string_view token_sv = lxl_lexer__peek_token(self);
     LXL_ASSERT(token_sv.length >= 1);
     if (token_sv.length == 1) {
         // Single-character tokens have their type as their value.
--- examples/c_lexer.h
+++ examples/c_lexer.h
@@ -1,137 +1,150 @@
 #ifndef LXL_EX_C_LEXER_H
 #define LXL_EX_C_LEXER_H
 
+#define C_TOKENS_DECLARE(CTOK, ...)             \
+    CTOK __VA_OPT__(= __VA_ARGS__),
+
+#define C_TOKENS_STRING_TABLE(CTOK, ...)        \
+    [CTOK] = LXL_SV_FROM_STRLIT_INIT(#CTOK),
+
+#define C_TOKENS(X)                                                     \
+    /* Indeintifer and literals. */                                     \
+    X(CTOK_IDENTIFIER)                    /* Identifer: a, hello_there, ... */ \
+    X(CTOK_LIT_INT)                       /* Integer literal: 42, 0xFF, 0ul, ... */ \
+    X(CTOK_LIT_FLOAT)                     /* Floating-point literal: 5.0, 3.14f, ... */ \
+    X(CTOK_LIT_CHAR)                      /* Character literal: 'H', 'i', ... */ \
+    X(CTOK_LIT_STRING)                    /* String literal: "Hello", "  world \xff", ... */ \
+    /* Brackets (equal to their ASCII values). */ \
+    X(CTOK_BKT_ROUND_LEFT, '(')           /* Left round bracket (parenthesis) */ \
+    X(CTOK_BKT_ROUND_RIGHT, ')')          /* Right round bracket (parenthesis) */ \
+    X(CTOK_BKT_CURLY_LEFT, '{')           /* Left curly bracket (brace) */ \
+    X(CTOK_BKT_CURLY_RIGHT, '}')          /* Right curly bracket (brace) */ \
+    X(CTOK_BKT_SQUARE_LEFT, '[')          /* Left square bracket */ \
+    X(CTOK_BKT_SQUARE_RIGHT, ']')         /* Right square bracket */ \
+    /* Single-character operators (equal to their ASCII values). */ \
+    X(CTOK_BANG, '!')                     /* Logical not */        \
+    X(CTOK_PERCENT, '%')                  /* Remainder */       \
+    X(CTOK_AMPERSAND, '&')                /* Address of, bitwise and */ \
+    X(CTOK_ASTERISK, '*')                 /* Dereference, multiply */ \
+    X(CTOK_PLUS, '+')                     /* Add */                \
+    X(CTOK_COMMA, ',')                    /* Comma operator */    \
+    X(CTOK_MINUS, '-')                    /* Subtract */          \
+    X(CTOK_DOT, '.')                      /* Member access */       \
+    X(CTOK_SLASH, '/')                    /* Divide */            \
+    X(CTOK_COLON, ':')                    /* "Else" part of ternary ?:, end of label. */ \
+    X(CTOK_SEMICOLON, ';')                /* Terminate statement */ \
+    X(CTOK_LT, '<')                       /* Less than */            \
+    X(CTOK_EQ, '=')                       /* Assign */               \
+    X(CTOK_GT, '>')                       /* Greater than */         \
+    X(CTOK_QMARK, '?')                    /* "Then" part of ternary ?: */ \
+    X(CTOK_BACKSLASH, '\\')               /* Conitnue line */ \
+    X(CTOK_CARET, '^')                    /* Bitwise exclusive or */ \
+    X(CTOK_VBAR, '|')                     /* Bitwise or */         \
+    X(CTOK_TILDE, '~')                    /* Bitwise not */       \
+    /* Multi-character operators. */            \
+    X(CTOK_BANG_EQ)                       /* !=  Not equal to */ \
+    X(CTOK_PERCENT_EQ)                    /* %=  Assign by remainder */ \
+    X(CTOK_AMPERSAND_AMPERSAND)           /* &&  Logical and */ \
+    X(CTOK_AMPERSAND_EQ)                  /* &=  Assign by bitwise and */ \
+    X(CTOK_ASTERISK_EQ)                   /* *=  Assign by multiplication */ \
+    X(CTOK_PLUS_PLUS)                     /* ++  Pre/post increment */ \
+    X(CTOK_PLUS_EQ)                       /* +=  Assign by addition */ \
+    X(CTOK_MINUS_MINUS)                   /* --  Pre-post decrement */ \
+    X(CTOK_MINUS_EQ)                      /* -=  Assign by subtraction */ \
+    X(CTOK_ARROW)                         /* ->  Member access by pointer */ \
+    X(CTOK_ELIPSIS)                       /* ... Variadic arguments */ \
+    X(CTOK_SLASH_EQ)                      /* /=  Assign by division */ \
+    X(CTOK_LT_LT)                         /* <<  Left bit shift */ \
+    X(CTOK_LT_LT_EQ)                      /* <<= Assign by left bit shift */ \
+    X(CTOK_LT_EQ)                         /* <=  Less than or equal to */ \
+    X(CTOK_EQ_EQ)                         /* ==  Equal to */ \
+    X(CTOK_GT_EQ)                         /* >=  Greater than or equal to */ \
+    X(CTOK_GT_GT)                         /* >>  Rigth bit shift */ \
+    X(CTOK_GT_GT_EQ)                      /* >>= Assign by right bit shift */ \
+    X(CTOK_CARET_EQ)                      /* ^=  Assign by bitwise exclusive or */ \
+    X(CTOK_VBAR_EQ)                       /* |=  Assign by bitwise or */ \
+    X(CTOK_VBAR_VBAR)                     /* ||  Logical or */ \
+    /* Keywords. */                             \
+    X(CTOK_KW_ALIGNAS)                    /* alignas, _Alignas */ \
+    X(CTOK_KW_ALIGNOF)                    /* alignof, _Alignof */ \
+    X(CTOK_KW_ATOMIC)                     /* _Atomic */ \
+    X(CTOK_KW_AUTO)                       /* auto */ \
+    X(CTOK_KW_BITINT)                     /* _BitInt */ \
+    X(CTOK_KW_BOOL)                       /* _Bool */ \
+    X(CTOK_KW_BREAK)                      /* break */ \
+    X(CTOK_KW_COMPLEX)                    /* _Complex */ \
+    X(CTOK_KW_CASE)                       /* case */ \
+    X(CTOK_KW_CHAR)                       /* char */ \
+    X(CTOK_KW_CONST)                      /* const */ \
+    X(CTOK_KW_CONSTEXPR)                  /* constexpr */ \
+    X(CTOK_KW_CONTINUE)                   /* continue */ \
+    X(CTOK_KW_DECIMAL128)                 /* _Decimal128 */ \
+    X(CTOK_KW_DECIMAL32)                  /* _Decimal32 */ \
+    X(CTOK_KW_DECIMAL64)                  /* _Decimal64 */ \
+    X(CTOK_KW_DEFAULT)                    /* default */ \
+    X(CTOK_KW_DO)                         /* do */ \
+    X(CTOK_KW_DOUBLE)                     /* double */ \
+    X(CTOK_KW_ELSE)                       /* else */ \
+    X(CTOK_KW_ENUM)                       /* enum */ \
+    X(CTOK_KW_EXTERN)                     /* extern */ \
+    X(CTOK_KW_FALSE)                      /* false */ \
+    X(CTOK_KW_FLOAT)                      /* float */ \
+    X(CTOK_KW_FOR)                        /* for */ \
+    X(CTOK_KW_GENERIC)                    /* _Generic */ \
+    X(CTOK_KW_GOTO)                       /* goto */ \
+    X(CTOK_KW_IF)                         /* if */ \
+    X(CTOK_KW_IMAGINARY)                  /* _Imaginary */ \
+    X(CTOK_KW_INLINE)                     /* inline */ \
+    X(CTOK_KW_INT)                        /* int */ \
+    X(CTOK_KW_LONG)                       /* long */ \
+    X(CTOK_KW_NORETURN)                   /* _Noreturn */ \
+    X(CTOK_KW_NULLPTR)                    /* nullptr */ \
+    X(CTOK_KW_REGISTER)                   /* register */ \
+    X(CTOK_KW_RESTRICT)                   /* restrict */ \
+    X(CTOK_KW_RETURN)                     /* return */ \
+    X(CTOK_KW_SHORT)                      /* short */ \
+    X(CTOK_KW_SIGNED)                     /* signed */ \
+    X(CTOK_KW_SIZEOF)                     /* sizeof */ \
+    X(CTOK_KW_STATIC)                     /* static */ \
+    X(CTOK_KW_STATIC_ASSERT)              /* static_assert, _Static_Assert */ \
+    X(CTOK_KW_STRUCT)                     /* struct */ \
+    X(CTOK_KW_SWITCH)                     /* switch */ \
+    X(CTOK_KW_THREAD_LOCAL)               /* thread_local, _Thread_Local */ \
+    X(CTOK_KW_TRUE)                       /* true */ \
+    X(CTOK_KW_TYPEDEF)                    /* typedef */ \
+    X(CTOK_KW_TYPEOF)                     /* typeof */ \
+    X(CTOK_KW_TYPEOF_UNQUAL)              /* typeof_unqual */ \
+    X(CTOK_KW_UNION)                      /* union */ \
+    X(CTOK_KW_UNSIGNED)                   /* unsigned */ \
+    X(CTOK_KW_VOID)                       /* void */ \
+    X(CTOK_KW_VOLATILE)                   /* volatile */ \
+    X(CTOK_KW_WHILE)                      /* while */ \
+
 enum c_token_type {
-    // Indeintifer and literals.
-    CTOK_IDENTIFIER,                    // Identifer: a, hello_there, ...
-    CTOK_LIT_INT,                       // Integer literal: 42, 0xFF, 0ul, ...
-    CTOK_LIT_FLOAT,                     // Floating-point literal: 5.0, 3.14f, ...
-    CTOK_LIT_CHAR,                      // Character literal: 'H', 'i', ...
-    CTOK_LIT_STRING,                    // String literal: "Hello", "  world\xff", ...
-    // Brackets (equal to their ASCII values).
-    CTOK_BKT_ROUND_LEFT                 = '(',  // Left round bracket (parenthesis)
-    CTOK_BKT_ROUND_RIGHT                = ')',  // Right round bracket (parenthesis)
-    CTOK_BKT_CURLY_LEFT                 = '{',  // Left curly bracket (brace)
-    CTOK_BKT_CURLY_RIGHT                = '}',  // Right curly bracket (brace)
-    CTOK_BKT_SQUARE_LEFT                = '[',  // Left square bracket
-    CTOK_BKT_SQUARE_RIGHT               = ']',  // Right square bracket
-    // Single-character operators (equal to their ASCII values).
-    CTOK_BANG                           = '!',  // Logical not
-    CTOK_PERCENT                        = '%',  // Remainder
-    CTOK_AMPERSAND                      = '&',  // Address of, bitwise and
-    CTOK_ASTERISK                       = '*',  // Dereference, multiply
-    CTOK_PLUS                           = '+',  // Add
-    CTOK_COMMA                          = ',',  // Comma operator
-    CTOK_MINUS                          = '-',  // Subtract
-    CTOK_DOT                            = '.',  // Member access
-    CTOK_SLASH                          = '/',  // Divide
-    CTOK_COLON                          = ':',  // "Else" part of ternary ?:, end of label.
-    CTOK_SEMICOLON                      = ';',  // Terminate statement
-    CTOK_LT                             = '<',  // Less than
-    CTOK_EQ                             = '=',  // Assign
-    CTOK_GT                             = '>',  // Greater than
-    CTOK_QMARK                          = '?',  // "Then" part of ternary ?:
-    CTOK_BACKSLASH                      = '\\', // Conitnue line
-    CTOK_CARET                          = '^',  // Bitwise exclusive or
-    CTOK_VBAR                           = '|',  // Bitwise or
-    CTOK_TILDE                          = '~',  // Bitwise not
-    // Multi-character operators.
-    CTOK_BANG_EQ,                       // !=  Not equal to
-    CTOK_PERCENT_EQ,                    // %=  Assign by remainder
-    CTOK_AMPERSAND_AMPERSAND,           // &&  Logical and
-    CTOK_AMPERSAND_EQ,                  // &=  Assign by bitwise and
-    CTOK_ASTERISK_EQ,                   // *=  Assign by multiplication
-    CTOK_PLUS_PLUS,                     // ++  Pre/post increment
-    CTOK_PLUS_EQ,                       // +=  Assign by addition
-    CTOK_MINUS_MINUS,                   // --  Pre-post decrement
-    CTOK_MINUS_EQ,                      // -=  Assign by subtraction
-    CTOK_ARROW,                         // ->  Member access by pointer
-    CTOK_ELIPSIS,                       // ... Variadic arguments
-    CTOK_SLASH_EQ,                      // /=  Assign by division
-    CTOK_LT_LT,                         // <<  Left bit shift
-    CTOK_LT_LT_EQ,                      // <<= Assign by left bit shift
-    CTOK_LT_EQ,                         // <=  Less than or equal to
-    CTOK_EQ_EQ,                         // ==  Equal to
-    CTOK_GT_EQ,                         // >=  Greater than or equal to
-    CTOK_GT_GT,                         // >>  Rigth bit shift
-    CTOK_GT_GT_EQ,                      // >>= Assign by right bit shift
-    CTOK_CARET_EQ,                      // ^=  Assign by bitwise exclusive or
-    CTOK_VBAR_EQ,                       // |=  Assign by bitwise or
-    CTOK_VBAR_VBAR,                     // ||  Logical or
-    // Keywords.
-    CTOK_KW_ALIGNAS,                    // alignas, _Alignas
-    CTOK_KW_ALIGNOF,                    // alignof, _Alignof
-    CTOK_KW_ATOMIC,                     // _Atomic
-    CTOK_KW_AUTO,                       // auto
-    CTOK_KW_BITINT,                     // _BitInt
-    CTOK_KW_BOOL,                       // _Bool
-    CTOK_KW_BREAK,                      // break
-    CTOK_KW_COMPLEX,                    // _Complex
-    CTOK_KW_CASE,                       // case
-    CTOK_KW_CHAR,                       // char
-    CTOK_KW_CONST,                      // const
-    CTOK_KW_CONSTEXPR,                  // constexpr
-    CTOK_KW_CONTINUE,                   // continue
-    CTOK_KW_DECIMAL128,                 // _Decimal128
-    CTOK_KW_DECIMAL32,                  // _Decimal32
-    CTOK_KW_DECIMAL64,                  // _Decimal64
-    CTOK_KW_DEFAULT,                    // default
-    CTOK_KW_DO,                         // do
-    CTOK_KW_DOUBLE,                     // double
-    CTOK_KW_ELSE,                       // else
-    CTOK_KW_ENUM,                       // enum
-    CTOK_KW_EXTERN,                     // extern
-    CTOK_KW_FALSE,                      // false
-    CTOK_KW_FLOAT,                      // float
-    CTOK_KW_FOR,                        // for
-    CTOK_KW_GENERIC,                    // _Generic
-    CTOK_KW_GOTO,                       // goto
-    CTOK_KW_IF,                         // if
-    CTOK_KW_IMAGINARY,                  // _Imaginary
-    CTOK_KW_INLINE,                     // inline
-    CTOK_KW_INT,                        // int
-    CTOK_KW_LONG,                       // long
-    CTOK_KW_NORETURN,                   // _Noreturn
-    CTOK_KW_NULLPTR,                    // nullptr
-    CTOK_KW_REGISTER,                   // register
-    CTOK_KW_RESTRICT,                   // restrict
-    CTOK_KW_RETURN,                     // return
-    CTOK_KW_SHORT,                      // short
-    CTOK_KW_SIGNED,                     // signed
-    CTOK_KW_SIZEOF,                     // sizeof
-    CTOK_KW_STATIC,                     // static
-    CTOK_KW_STATIC_ASSERT,              // static_assert, _Static_Assert
-    CTOK_KW_STRUCT,                     // struct
-    CTOK_KW_SWITCH,                     // switch
-    CTOK_KW_THREAD_LOCAL,               // thread_local, _Thread_Local
-    CTOK_KW_TRUE,                       // true
-    CTOK_KW_TYPEDEF,                    // typedef
-    CTOK_KW_TYPEOF,                     // typeof
-    CTOK_KW_TYPEOF_UNQUAL,              // typeof_unqual
-    CTOK_KW_UNION,                      // union
-    CTOK_KW_UNSIGNED,                   // unsigned
-    CTOK_KW_VOID,                       // void
-    CTOK_KW_VOLATILE,                   // volatile
-    CTOK_KW_WHILE,                      // while
+    C_TOKENS(C_TOKENS_DECLARE)
 };
 
-// Match a word-constituent character to C tokens -- for identifiers and keywords: [A-Za-z0-9_].
+// Create a lexer capable of lexing C code.
+struct lxl_lexer create_c_lexer(struct lxl_string_view src);
+
+// Return a string view of the name for the given C token type.
+struct lxl_string_view c_token_kind_name(enum c_token_type type);
+
+// Match an initial word-constituent character for C tokens -- [A-Za-z_].
+bool match_word_init_char(struct lxl_lexer *self);
+
+// Match a word-constituent character for C tokens -- for identifiers and keywords: [A-Za-z0-9_].
 bool match_word_char(struct lxl_lexer *self);
 
 // Match an integer literal prefix -- (digit) != 0: decimal, 0: octal, 0x/0X: hexadecimal, 0b/0B: binary.
 bool match_int_prefix(struct lxl_lexer *self);
 
-// Match an integer literal digit -- [0-9A-Fa-f']: this does not care about the prefix.
-bool match_int_digit(struct lxl_lexer *self);
-
-// Match a floating-point literal prefix : ., 0x for hexadecimal, or any digit for decimal.
-bool match_float_prefix(struct lxl_lexer *self);
-
 // Match a punct token completely.
 bool match_punct(struct lxl_lexer *self);
 
 int get_word_type(struct lxl_lexer *self);
-
+int get_int_type(struct lxl_lexer *self);
+int get_float_type(struct lxl_lexer *self);
 int get_punct_type(struct lxl_lexer *self);
 
 #endif
