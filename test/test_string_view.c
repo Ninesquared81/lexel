922e9cd7a90807db47e9b24f2f37121f4887fbcd
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2026-06-07 00:49:30 +0100

    Add tests for `sv_has_prefix`/`suffix()`

--- test/test_string_view.c
+++ test/test_string_view.c
@@ -7,6 +7,14 @@ int main(void) {
            lxl_sv_eq(LXL_SV_FROM_STRLIT("abc"), LXL_SV_FROM_STRLIT("abc")));
     printf("abc == cba: %d (expected: 0)\n",
            lxl_sv_eq(LXL_SV_FROM_STRLIT("abc"), LXL_SV_FROM_STRLIT("cba")));
+    printf("abc has prefix ab: %d (expected: 1)\n",
+           lxl_sv_has_prefix(LXL_SV_FROM_STRLIT("abc"), LXL_SV_FROM_STRLIT("ab")));
+    printf("abc has suffix ab: %d (expected: 0)\n",
+           lxl_sv_has_suffix(LXL_SV_FROM_STRLIT("abc"), LXL_SV_FROM_STRLIT("ab")));
+    printf("abc has prefix bc: %d (expected: 0)\n",
+           lxl_sv_has_prefix(LXL_SV_FROM_STRLIT("abc"), LXL_SV_FROM_STRLIT("bc")));
+    printf("abc has suffix bc: %d (expected: 1)\n",
+           lxl_sv_has_suffix(LXL_SV_FROM_STRLIT("abc"), LXL_SV_FROM_STRLIT("bc")));
     // printf("ab <=> abc: %d (expected: -1)\n",
     //        lxl_sv_compare(LXL_SV_FROM_STRLIT("ab"), LXL_SV_FROM_STRLIT("abc")));
     // printf("ab <=> a: %d (expected: 1)\n",
